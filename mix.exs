3055f4c6896d2dbffc2b555f313706f16ee0ab28
Alex Koutmos <akoutmos@gmail.com>
2026-02-19 18:34:25 -0600

    Adding new project boilerplate stuff

--- mix.exs
+++ mix.exs
@@ -4,10 +4,17 @@ defmodule Fred.MixProject do
   def project do
     [
       app: :fred,
-      version: "0.1.0",
+      version: project_version(),
       elixir: "~> 1.18",
+      name: "Fred",
+      source_url: "https://github.com/akoutmos/fred",
+      homepage_url: "https://hex.pm/packages/fred",
+      description: "An Elixir client for the Federal Reserve Economic Data API",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      docs: docs(),
+      aliases: aliases()
     ]
   end
 
@@ -18,11 +25,67 @@ defmodule Fred.MixProject do
     ]
   end
 
+  defp package do
+    [
+      name: "fred",
+      files: ~w(lib mix.exs README.md LICENSE CHANGELOG.md VERSION),
+      licenses: ["MIT"],
+      maintainers: ["Alex Koutmos"],
+      links: %{
+        "GitHub" => "https://github.com/akoutmos/fred",
+        "Sponsor" => "https://github.com/sponsors/akoutmos"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_ref: "master",
+      logo: "guides/images/logo.png",
+      extras: ["README.md"]
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # Required dependencies
+      {:req, "~> 0.5"},
+      {:telemetry, "~> 1.3"},
+
+      # Optional dependencies
+      {:geo, "~> 3.6 or ~> 4.0", optional: true},
+
+      # Development deps
+      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: :dev},
+      {:doctor, "~> 0.22", only: :dev}
     ]
+  end
+
+  defp aliases do
+    [
+      docs: ["docs", &copy_files/1]
+    ]
+  end
+
+  defp project_version do
+    "VERSION"
+    |> File.read!()
+    |> String.trim()
+  end
+
+  defp copy_files(_) do
+    # Set up directory structure
+    File.mkdir_p!("./doc/guides/images")
+
+    # Copy over image files
+    "./guides/images/"
+    |> File.ls!()
+    |> Enum.each(fn image_file ->
+      File.cp!("./guides/images/#{image_file}", "./doc/guides/images/#{image_file}")
+    end)
   end
 end
